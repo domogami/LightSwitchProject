9ffd704eff62917cc49d5ad536f9e9ec24621085
Dominick <d3lee@ucsd.edu>
2019-05-28 21:23:03 -0700

    pages

--- App.js
+++ App.js
@@ -1,101 +1,61 @@
 import React from 'react';
-import {StyleSheet, Linking, Text, View, Button, Alert, AppRegistry, TextInput} from 'react-native';
+import { Button, View, Text } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
-export default class App extends React.Component {
-
-    constructor() {
-        super()
-        this.state = {
-            value: "Edit Me!"
-        }
-        this.handleChangeText = this.handleChangeText.bind(this)
-    }
-
-    handleChangeText(newText) {
-        this.setState({
-            value: newText
-        })
-    }
-
-    onPress() {
-
-        fetch('http://192.168.43.6', {
-            method: 'GET',
-            headers: {
-                Accept: '/turn/on',
-                'Content-Type': '/turn/on',
-            },
-        })
-    }
-
-    onPress1() {
-        fetch('http://192.168.43.6', {
-            method: 'GET',
-            headers: {
-                Accept: '/turn/off',
-                'Content-Type': '/turn/off',
-            },
-        })
-    }
-
+class HomeScreen extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <TextInput
-                    defaultValue={this.state.value}
-                    onChangeText={this.handleChangeText}
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
                 />
-                <Text style={styles.titleText}> {this.state.value} </Text>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        onPress={this.onPress}
-                        title="Hello"
-                        color="#FFFFFF"
-                        accessibilityLabel="Tap on Me"
-                        fontSize="30"
-                    />
-                </View>
-
-                <View style={styles.buttonContainer}>
-                    <Button
-                        onPress={this.onPress1}
-                        title="Hello2"
-                        color="#FFFFFF"
-                        accessibilityLabel="Tap on Me"
-                        fontSize="30"
-                    />
-                </View>
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonContainer: {
-        backgroundColor: '#2E9298',
-        borderRadius: 10,
-        padding: 10,
-        shadowColor: '#000000',
-        shadowOffset: {
-            width: 0,
-            height: 3
-        },
-        shadowRadius: 10,
-        shadowOpacity: 0.25
-    },
-    customBtnTxt: {
-        fontSize: 40,
-        fontWeight: '400',
-        color: "#fff",
-    },
-    titleText: {
-        fontSize: 20,
-        fontWeight: 'bold',
+class DetailsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Details Screen</Text>
+                <Button
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('Details')}
+                />
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                />
+            </View>
+        );
     }
-});
+}
+
+const RootStack = createStackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+        },
+        Details: {
+            screen: DetailsScreen,
+        },
+    },
+    {
+        initialRouteName: 'Home',
+    }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+    render() {
+        return <AppContainer />;
+    }
+}
